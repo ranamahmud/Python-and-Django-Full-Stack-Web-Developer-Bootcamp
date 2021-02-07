ffd85f2e6ed2ba4f268b27b301e130bc9ebeebde
ranamahmud <ranamd0@gmail.com>
2021-02-07 16:55:41 +0600

    javascript started

---
+++ 14 Python Level One/94 dictionary.py
@@ -0,0 +1,10 @@
+my_stuff = {"key1": "value1", "key2": "value2",'key3':{'123':[1,2,'grabMe']}}
+print(my_stuff)
+print(my_stuff['key1'])
+print(my_stuff['key2'])
+print(my_stuff['key3']['123'][2].upper())
+
+my_stuff = {'lunch':'pizza','breakfast':'egggs'}
+my_stuff['lunch'] = 'burger'
+my_stuff['dinner'] = 'chicken'
+print(my_stuff)
---
+++ 9 JavaScript Level One/50 level one.js
@@ -0,0 +1,46 @@
+10
+10
+20.2
+20.2
+    - 13.4
+    - 13.4
+"Hello World"
+"Hello world"
+"10"
+"10"
+true
+false
+undefined
+null
+
+console.log(2 + 2)
+console.log(25 - 2)
+console.log(24 * 2)
+console.log(25 / 2)
+console.log(15 % 14)
+
+console.log("Django is cool")
+console.log("Django is cool".length)
+console.log("Hello\n start a new \tline")
+console.log("Hello"[0])
+console.log("Hello"[1])
+console.log("Hello"[2])
+console.log("Hello"[3])
+
+var bankAccount = 100;
+console.log(bankAccount)
+var deposit = 50;
+var total = bankAccount + deposit;
+console.log(total)
+
+var greeting = 'Welcome back: ';
+var name = "Rana";
+console.log(greeting + name)
+var myVariable;
+console.log(myVariable)
+
+var bonus = null;
+console.log(bonus);
+
+// var age = prompt("How old are you?")
+// console.log(age)
